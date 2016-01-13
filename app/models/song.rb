1ea233ebe3282d414a224f2879996419c990488a
kieranpringle <contact@kieranpringle.com>
2016-01-13 15:31:11 +0000

    initial commit

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song
   include Mongoid::Document
+
+  belongs_to :user
+
+  field :title, type: String, default: ""
+  			
+
+  field :sequencers, type: Hash, default: {}
+  field :parameters, type: Hash, default: {}
+
 end
